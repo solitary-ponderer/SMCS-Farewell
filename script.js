1154332e414b73fe2044564972c360b4606bd2f2
solitary-ponderer <arkonilsarkar.24@gmail.com>
2024-12-08 14:29:08 +0530

    Mobile Responsiveness Update 1.o

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-//Mouse movement
-document.addEventListener("mousemove", (event) => {
-    // Collecting cursor coordinate
-    const x = event.clientX / window.innerWidth;
-    const y = event.clientY / window.innerHeight;
+// Check if the device is mobile
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-    // Updating the co ordinates
-    document.body.style.setProperty("--x", `${x * 100}%`);
-    document.body.style.setProperty("--y", `${y * 100}%`);
-});
+if (!isMobile) {
+    //Mouse movement for PC
+    document.addEventListener("mousemove", (event) => {
+        const x = event.clientX / window.innerWidth;
+        const y = event.clientY / window.innerHeight;
+
+        // Update the coordinates for the cursor effect
+        document.body.style.setProperty("--x", `${x * 100}%`);
+        document.body.style.setProperty("--y", `${y * 100}%`);
+    });
+} else {
+    // Disable background effect on mobile
+    document.body.style.setProperty("--x", "50%");
+    document.body.style.setProperty("--y", "50%");
+}
